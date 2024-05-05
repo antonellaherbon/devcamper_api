280f25d349d6954c5741c7ab8546cfc0d632a348
Antonella <antoh2003@gmail.com>
2024-05-05 19:14:49 -0300

    async middleware

--- controllers/data.js
+++ controllers/data.js
@@ -1,24 +1,23 @@
 const Data = require('../modals/Data');
 const ErrorResponse = require ('../utils/errorResponse');
+const asyncHandler = require('../middleware/async')
 //@desc  Get All Data
 //@route GET  /api/v1/data
 //@access Public 
 
-exports.getData = async (req,res, next) => {
-    try {
+exports.getData = asyncHandler(async (req,res, next) => {
+
         const data = await Data.find();
         res.status(200).json({success: true,count: data.length, data: data});
-    } catch (error) {
+        
         next(error);
-    }
-};
+});
 
 //@desc  Get Single Data
 //@route GET  /api/v1/data/:id
 //@access Public 
 
-exports.getSingleData = async (req, res, next) => {
-    try {
+exports.getSingleData = asyncHandler( async (req, res, next) => {
         const singleData = await Data.findById(req.params.id);
 
         if (!singleData) {
@@ -26,36 +25,32 @@ exports.getSingleData = async (req, res, next) => {
                 new ErrorResponse(`Data not found with id of ${req.params.id}`,
                 404)
             );
-        }
+        };
         res.status(200).json({ success: true, data: singleData });
-    } catch (error) {
+        
         next(error);
-    }
-};
+});
 
 //@desc  Create Data
 //@route POST  /api/v1/data
 //@access Private 
 
-exports.createData = async (req,res, next) => {
-    try {
+exports.createData = asyncHandler( async (req,res, next) => {
         const data = await Data.create(req.body);
         console.log(req.body)
         res.status(201).json({
             success: true,
             data: data
         });
-    } catch (error) {
+
         next(error);
-    }
-};
+});
 
 //@desc  Update Data
 //@route PUT  /api/v1/data/:id
 //@access Private 
 
-exports.updateData = async (req,res, next) => {
-    try {
+exports.updateData = asyncHandler( async (req,res, next) => {
         const singleData = await Data.findByIdAndUpdate(req.params.id, req.body,{
             new: true,
             runValidators: true 
@@ -66,19 +61,17 @@ exports.updateData = async (req,res, next) => {
                 new ErrorResponse(`Data not found with id of ${req.params.id}`,
                 404)
             );
-        }
+        };
+
         res.status(200).json({success: true, data: singleData});
-    } catch (error) {
         next(error);
-    }
-};
+});
 
 //@desc  Delete Data
 //@route DELETE  /api/v1/data/:id
 //@access Private 
 
-exports.deleteData = async (req,res, next) => {
-    try {
+exports.deleteData = asyncHandler( async (req,res, next) => {
         const singleData = await Data.findByIdAndDelete(req.params.id);
     
         if (!singleData){
@@ -86,12 +79,11 @@ exports.deleteData = async (req,res, next) => {
                 new ErrorResponse(`Data not found with id of ${req.params.id}`,
                 404)
             );
-        }
+        };
+
         res.status(200).json({success: true, data: {}});
-    } catch (error) {
         next(error);
-    }
-};
+});
 
 
 
