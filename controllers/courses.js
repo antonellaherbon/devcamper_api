8a9c1ebedb197eaff7daef2282a91509deda035d
Antonella <antoh2003@gmail.com>
2024-05-09 23:18:21 -0300

    routes

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const Course = require('../modals/Course');
 const ErrorResponse = require ('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const Data = require('../modals/Data');
 
 
 //@desc  Get All Courses
@@ -27,5 +28,52 @@ exports.getCourses = asyncHandler (async (req, res, next) => {
         count: courses.length,
         data: courses
     })
+});
+
+
+//@desc  Get Single Course
+//@route GET  /api/v1/courses/:id
+//@access Public 
+
+exports.getSingleCourse = asyncHandler (async (req, res, next) => {
+    const course = await Course.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    });
+
+    if (!course){
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 
+        404)
+    }
+
+    res.status(200).json({
+        success: true,
+        data: course
+    })
+});
+
+//@desc  Add a course
+//@route POST  /api/v1/data/:dataId/courses
+//@access Private 
+
+exports.addCourse = asyncHandler (async (req, res, next) => {
+    req.body.bootcamp = req.params.dataId;
+    
+    const data = await Data.findById(req.params.dataId);
+
+    if (!data){
+        return next(new ErrorResponse(`No course with the id of ${req.params.dataId}`), 
+        404)
+    };
+
+    const course = await Course.create(req.body);
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
 })
+
+
+
 
