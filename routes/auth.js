b769f110c8c6e3e2017ced999e2aab8bd484169b
Antonella <antoh2003@gmail.com>
2024-05-14 15:34:16 -0300

    authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/auth');
+const { registerUser, loginUser, getMe } = require('../controllers/auth');
 
 const router = express.Router();
 
+const { protect } = require('../middleware/auth');
+
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-
+router.get('/me', protect, getMe);
 
 module.exports = router;
