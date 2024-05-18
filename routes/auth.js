2493e4c5bf3c7a5e6b4411384b489206551a6ead
Antonella <antoh2003@gmail.com>
2024-05-18 16:49:40 -0300

    log out controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
+const { registerUser, loginUser, getMe, forgotPassword, resetPassword, updateDetails, updatePassword, logOut } = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/logout', logOut);
 router.get('/me', protect, getMe);
 router.put('/updateDetails', protect, updateDetails);
 router.put('/updatePassword', protect, updatePassword);
